5ca80c47a04f4daa5b9802da13ce461289e37e07
liguoqinjim <liguoqinjim23@gmail.com>
2017-10-20 20:53:50 +0800

    damai_login

--- damai_login/damai_login.js
+++ damai_login/damai_login.js
@@ -1,45 +1,26 @@
-var page = require('webpage').create(),
-  testindex = 0,
-  loadInProgress = false;
+var page = require('webpage').create();
+var fs = require('fs');
+
+var content = fs.read("./damai_login/user.json");
+var userData = JSON.parse(content);
+var username = userData["username"];
+var password = userData["password"];
+
+if (username == "" || password == "") {
+  console.log("userData error");
+  phantom.exit();
+}
 
 page.onLoadStarted = function () {
-  loadInProgress = true;
   console.log("load started");
 };
 
 page.onLoadFinished = function () {
-  loadInProgress = false;
   console.log("load finished");
 };
 
-var steps = [
-  function () {
-    page.open("https://secure.damai.cn/login.aspx");
-  },
+page.open("https://secure.damai.cn/login.aspx", function (status) {
+  page.render('./damai_login/login_damai.png');
 
-  function () {
-    page.evaluate(function (obj) {
-      // var form = document.getElementById("login-main-form");
-      // form.elements["username"].value = '用户名';
-      // form.elements["pwd"].value = '密码';
-      // form.elements['login-button'].click();
-      //
-      // return document.title;
-    });
-    loadInProgress = true;
-  },
-
-  function () {
-    page.render('./damai_login/login_damai.png');
-  }
-];
-
-var interval = setInterval(function () {
-  if (!loadInProgress && typeof steps[testindex] == "function") {
-    steps[testindex]();
-    testindex++;
-  }
-  if (typeof steps[testindex] != "function") {
-    phantom.exit();
-  }
-}, 10);
+  phantom.exit();
+});
