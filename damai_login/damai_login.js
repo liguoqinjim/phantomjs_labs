0c6de680ec40f7631f52f9db97af22a6fcb41fa3
liguoqinjim <liguoqinjim23@gmail.com>
2017-10-21 21:02:15 +0800

    damai_login 模拟登录完成

--- damai_login/damai_login.js
+++ damai_login/damai_login.js
@@ -7,20 +7,35 @@ var username = userData["username"];
 var password = userData["password"];
 
 if (username == "" || password == "") {
-  console.log("userData error");
-  phantom.exit();
+    console.log("userData error");
+    phantom.exit();
 }
 
 page.onLoadStarted = function () {
-  console.log("load started");
+    console.log("load started");
 };
 
 page.onLoadFinished = function () {
-  console.log("load finished");
+    console.log("load finished");
+};
+
+page.onConsoleMessage = function (msg) {
+    console.log("console:" + msg);
 };
 
 page.open("https://secure.damai.cn/login.aspx", function (status) {
-  page.render('./damai_login/login_damai.png');
+    page.includeJs("https://cdn.bootcss.com/jquery/1.6.4/jquery.min.js", function () {
+        var rect = page.evaluate(function (username, password) {
+            $('#login_email').val(username);
+            $('#login_pwd_txt').trigger("focus");
+            $('#login_pwd').val(password);
+            $('#subbtn').click();
+        }, username, password);
 
-  phantom.exit();
+        console.log("rect=" + rect);
+        var clock = setTimeout(function () {
+            page.render('./damai_login/login_damai.png');
+            phantom.exit();
+        }, 3000);
+    });
 });
