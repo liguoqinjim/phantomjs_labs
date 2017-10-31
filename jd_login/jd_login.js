14a86e4dcd8266a348471ef898ca6cc473f56bba
liguoqinjim <liguoqinjim23@gmail.com>
2017-10-31 21:00:43 +0800

    jd_login 模拟点击

--- jd_login/jd_login.js
+++ jd_login/jd_login.js
@@ -1,3 +1,4 @@
+var page = require('webpage').create();
 var fs = require('fs');
 
 //读取用户名和密码
@@ -5,3 +6,52 @@ var content = fs.read("./jd_login/user.json");
 var userData = JSON.parse(content);
 var username = userData["username"];
 var password = userData["password"];
+
+//截屏index
+var pageRenderIndex = 0;
+
+//乱码
+phantom.outputEncoding = "gbk";
+
+//设置页面大小
+page.viewportSize = {width: 1366, height: 768};
+
+//截图方法
+function renderPage() {
+    page.render('./jd_login/screenshot/jd_login' + pageRenderIndex + '.png');
+    pageRenderIndex++;
+}
+
+//页面加载完成
+page.onLoadFinished = function (status) {
+    // page.render('./jd_login/jd_login.png');
+    //
+    // console.log("截屏完成");
+
+    renderPage();
+
+    // phantom.exit();
+};
+
+page.open("https://passport.jd.com/new/login.aspx?ReturnUrl=https%3A%2F%2Fwww.jd.com%2F", function (status) {
+    page.includeJs("https://cdn.bootcss.com/jquery/1.6.4/jquery.min.js", function () {
+        page.evaluate(function () {
+            $('.login-tab').click();
+        });
+
+        renderPage();
+
+        // var rect = page.evaluate(function (username, password) {
+        //     $('#login_email').val(username);
+        //     $('#login_pwd_txt').trigger("focus");
+        //     $('#login_pwd').val(password);
+        //     $('#subbtn').click();
+        // }, username, password);
+
+        // console.log("rect=" + rect);
+        var clock = setTimeout(function () {
+            // renderPage();
+            phantom.exit();
+        }, 3000);
+    });
+});
